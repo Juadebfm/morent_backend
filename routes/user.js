bf3ce5737d989abe7a2b0358593bc403c53918ff
Julius Adebowale Gabriel <juadebgabriel@gmail.com>
2024-08-17 13:36:13 +0100

    fix

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,59 @@
 const express = require("express");
+const {
+  verifyToken,
+  verifyTokenAndAdmin,
+  verifyTokenAndAuthorization,
+} = require("../middleware/verifyToken");
+const User = require("../models/User");
+const CryptoJS = require("crypto-js");
 
 const router = express.Router();
 
 // Update User
-router.put("/:id", async (req, res) => {});
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  // Confirming user password as another layer of the securityu, check user password for situation where the user has changed it.
+  if (req.body.password) {
+    req.body.password = CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString();
+  }
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+
+      { new: true }
+    );
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// Delete user
+router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json({ message: "User Has Been Deleted" });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+//Get Single User
+router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    const { password, ...others } = user._doc;
+
+    res.status(200).json(others);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 module.exports = router; // Export the router
