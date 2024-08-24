4eaafb7873717f5ce028c42de37337e4a87433c3
Julius Adebowale Gabriel <juadebgabriel@gmail.com>
2024-08-24 12:45:37 +0100

    Done with car route

--- routes/car.js
+++ routes/car.js
@@ -1,9 +1,5 @@
 const express = require("express");
-const {
-  verifyToken,
-  verifyTokenAndAdmin,
-  verifyTokenAndAuthorization,
-} = require("../middleware/verifyToken");
+const { verifyTokenAndAdmin } = require("../middleware/verifyToken");
 const Car = require("../models/Car");
 
 const router = express.Router();
@@ -34,6 +30,52 @@ router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
     res.status(200).json(updatedCar);
   } catch (error) {
     res.status(500).json(error);
+  }
+});
+
+//delete car
+router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    await Car.findByIdAndDelete(req.params.id);
+    res.status(200).json({ message: "Car Has Been Deleted" });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// Get a single car
+router.get("/find/:id", async (req, res) => {
+  try {
+    const car = await Car.findById(req.params.id);
+
+    res.status(200).json(car);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// Get All Cars
+router.get("/", async (req, res) => {
+  const queryNew = req.query.new;
+  const queryCarType = req.query.carType;
+
+  try {
+    let cars;
+    if (queryNew === "true") {
+      cars = await Car.find().sort({ createdAt: -1 }).limit(5);
+    } else if (queryCarType) {
+      cars = await Car.find({
+        carType: { $regex: new RegExp(queryCarType, "i") },
+      });
+    } else {
+      cars = await Car.find();
+    }
+
+    res.status(200).json(cars);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "An error occurred", error: error.message });
   }
 });
 
