35bd40adc93689d372651dd44104163ca913d9de
Julius Adebowale Gabriel <juadebgabriel@gmail.com>
2024-09-19 23:01:22 +0100

    fix

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,19 +1,73 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../Navbar/Navbar.scss";
 import { CiSearch } from "react-icons/ci";
 import { VscSettings } from "react-icons/vsc";
 import { IoIosNotifications, IoMdHeart, IoMdSettings } from "react-icons/io";
 import { userprofile } from "../../assets/assets";
+import { useAuth } from "../../context/authContext";
+import { useCars } from "../../context/carContext";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  const [likedCars, setLikedCars] = useState([]);
+
+  // contexts
+  const { user } = useAuth();
+  const { cars } = useCars();
+  const navigate = useNavigate();
+
+  //Liked cars and iother functionalities
+  useEffect(() => {
+    const storedLikedCars = JSON.parse(localStorage.getItem("likedCars")) || {};
+    setLikedCars(storedLikedCars);
+    console.log(storedLikedCars);
+
+    const checkForNewCars = async () => {
+      try {
+        const response = await fetch(
+          "https://morent-backend-zeta.vercel.app/api/cars/count"
+        );
+      } catch (error) {
+        console.log(error);
+      }
+    };
+  }, []);
+
+  // filter cars
+  useEffect(() => {
+    if (searchTerm) {
+      const filteredCars = cars.filter(
+        (car) =>
+          car.carName.toLowerCase().includes(searchTerm.toLowerCase()) ||
+          car.carType.toLowerCase().includes(searchTerm.toLowerCase()) ||
+          car.price.toString().includes(searchTerm) ||
+          car.transmission.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+      setSearchResults(filteredCars);
+    } else {
+      setSearchResults([]);
+    }
+  }, [searchTerm, cars]);
+
+  const handleSearchChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const handleCarSelect = (carId) => {
+    navigate(`/car/${carId}`);
+    setSearchTerm("");
+    setSearchResults([]);
+  };
   return (
     <nav>
       <div className="navbar_logo_search">
         <span>Morent</span>
         <div>
-          <CiSearch className="navbar_logo_search_icon"/>
+          <CiSearch className="navbar_logo_search_icon" />
           <input type="text" placeholder="Search Something Here" />
-          <VscSettings className="navbar_logo_search_icon"/>
+          <VscSettings className="navbar_logo_search_icon" />
         </div>
       </div>
 
--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { CarProvider } from "./context/carContext";
+import { AuthProvider } from "./context/authContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <CarProvider>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </CarProvider>
   </React.StrictMode>
 );
