5ceeb9b350ca13f05373d1fbc08a45e7456ad335
freshovie <egorkeovie@gmail.com>
2024-10-29 14:15:22 +0100

    ovie

--- client/src/assets/dateUtils.js
+++
@@ -1,5 +0,0 @@
-export function getCurrentDate() {
-  const now = new Date();
-  const options = { year: "numeric" };
-  return now.toLocaleDateString("en-US", options);
-}
--- client/src/pages/CarPayment/CarPayment.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const CarPayment = () => {
-  return <div>CarPayment</div>;
-};
-
-export default CarPayment;
